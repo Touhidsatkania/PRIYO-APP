5e13f0586defde48f6d95466a001dc347c27494b
Touhidsatkania <touhidsatkania@gmail.com>
2026-08-25 22:05:07 +0300

    Refactor WebView settings and URL loading
    Removed window title request and adjusted URL loading behavior in WebView.

--- app/src/main/java/com/priyo/app/MainActivity.java
+++ app/src/main/java/com/priyo/app/MainActivity.java
@@ -2,7 +2,6 @@ package com.priyo.app;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.Window;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -19,40 +18,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         webView = new WebView(this);
-
-        WebView.setWebContentsDebuggingEnabled(false);
 
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(
                     WebView view,
                     WebResourceRequest request) {
-
-                view.loadUrl(request.getUrl().toString());
-                return true;
+                return false;
             }
 
             @Override
             public boolean shouldOverrideUrlLoading(
                     WebView view,
                     String url) {
-
-                view.loadUrl(url);
-                return true;
+                return false;
             }
         });
 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setDatabaseEnabled(true);
+
         webView.getSettings().setBuiltInZoomControls(false);
         webView.getSettings().setDisplayZoomControls(false);
         webView.getSettings().setSupportZoom(false);
-        webView.getSettings().setLoadWithOverviewMode(false);
-        webView.getSettings().setUseWideViewPort(false);
 
         webView.setVerticalScrollBarEnabled(false);
         webView.setHorizontalScrollBarEnabled(false);
